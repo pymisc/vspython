77fa7cee89bfab03b3f4da699959a8d25aa3be53
pymisc <vs95054@gmail.com>
2023-02-02 17:40:15 -0800

    Updated 5:40 PM

---
+++ prog8.py
@@ -0,0 +1,20 @@
+from tkinter import ttk
+from tkinter import *
+
+root = Tk()
+root.title('Basic sum program for 1st graders')
+root.geometry('1250x400')
+root.configure(bg='gray')
+
+# Combobox creation
+n = StringVar()
+monthchoosen = ttk.Combobox(root, width = 27, textvariable = n, font=('Times 36'))
+  
+# Adding combobox drop down list
+monthchoosen['values'] = (' January', ' February', ' March', ' April', ' May', 
+    ' June', ' July',  'August', ' September', ' October', ' November', ' December')
+  
+monthchoosen.grid(column = 1, row = 5)
+monthchoosen.current()
+
+root.mainloop()
