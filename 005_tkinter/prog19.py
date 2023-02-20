176d89509b70fe7c9b3f91af4aa563ff0c5db517
pymisc <vs95054@gmail.com>
2023-02-20 09:11:42 -0800

    updated

--- 005_tkinter/prog19.py
+++ 005_tkinter/prog19.py
@@ -8,6 +8,6 @@ canvas = tk.Canvas(root, width=300, height=200)
 canvas.pack()
 
 # create a line with thickness of 2 pixels
-line = canvas.create_line(50, 50, 250, 50, width=2)
+line = canvas.create_line(50, 50, 250, 50, width=2, fill='blue')
 
 root.mainloop()
