7c3ee25d03f1fcad9b35318c3c2b22dcfb05b6db
pymisc <vs95054@gmail.com>
2024-09-22 07:00:15 -0700

    basic operators testing

---
+++ 001_basics/00_operators_and_printing.py
@@ -0,0 +1,10 @@
+# Just to test some basic printing in python
+
+# operators testing
+print (2*3)     # multiplication operator
+print (9/2)     # division operator
+print (9 // 2)  # floor division operator 
+print (9 % 2)   # modulus operator - for remainder
+
+# escaping ' in print statement
+print ('vikas\'s "laptop"')
