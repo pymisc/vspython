4d1de6794c41c4efeadf6cd2df35a0f76ee0fa85
pymisc <vs95054@gmail.com>
2024-09-22 07:04:31 -0700

    new additions

--- 001_basics/00_operators_and_printing.py
+++ 001_basics/00_operators_and_printing.py
@@ -1,6 +1,7 @@
 # Just to test some basic printing in python
 
 # operators testing
+print (3+2)     # addition operator
 print (2*3)     # multiplication operator
 print (9/2)     # division operator
 print (9 // 2)  # floor division operator 
