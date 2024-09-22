5fe81f2d1309f534482ea588d0a65ce9e65ed6e8
pymisc <vs95054@gmail.com>
2024-09-22 07:22:30 -0700

    New code

--- 001_basics/00_operators_and_printing.py
+++ 001_basics/00_operators_and_printing.py
@@ -1,11 +1,21 @@
 # Just to test some basic printing in python
 
 # operators testing
-print (3+2)     # addition operator
-print (2*3)     # multiplication operator
-print (9/2)     # division operator
-print (9 // 2)  # floor division operator 
-print (9 % 2)   # modulus operator - for remainder
+print ("Addition       \t 3+2 \t =", 3+2)     # addition operator
+print ("Multiplication \t 2*3 \t =", 2*3)     # multiplication operator
+print ("Division       \t 9/2 \t =", 9/2 )     # division operator
+print ("Exponent       \t 2**3 \t =", 2 ** 3)    # exponent operator
+print ("Floor div      \t 9 // 2 \t =", 9 // 2)  # floor division operator 
+print ("Modulus        \t 9 % 2 \t =", 9 % 2)   # modulus operator - for remainder
 
 # escaping ' in print statement
 print ('vikas\'s "laptop"')
+
+print("Using variables")
+x = 2
+y = 3
+print (x+y)
+
+z = x + y
+#p = _ + 10
+#print (p)
