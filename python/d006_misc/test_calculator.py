8a5f312aff9f496e412bb9408569c6ca3eb25148
pymisc <vs95054@gmail.com>
2026-07-17 12:17:14 -0700

    updated test case

--- python/d006_misc/test_calculator.py
+++ python/d006_misc/test_calculator.py
@@ -1,9 +1,16 @@
 # test_calculator.py
-from .calculator import add_numbers
+# from .calculator import add_numbers
+
+# def test_add_numbers():
+#     # Test that 2 + 3 equals 5
+#     assert add_numbers(2, 3) == 5
+    
+#     # Test that -1 + 1 equals 0
+#     assert add_numbers(-1, 1) == 0
+
+# test_calculator.py
+from python.d006_misc.calculator import add_numbers
 
 def test_add_numbers():
-    # Test that 2 + 3 equals 5
     assert add_numbers(2, 3) == 5
-    
-    # Test that -1 + 1 equals 0
     assert add_numbers(-1, 1) == 0
